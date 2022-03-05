30904db1bfb374836453e7d594469f30cab9a092
Ryan Conn <rconn478@gmail.com>
2022-03-05 12:28:48 -0600

    Add realblur dataset support

--- data_loader.py
+++ data_loader.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import tensorflow as tf
 
-def get_dataset_item(filename):
+def get_exposure_dataset_item(filename):
     # Load image
     image = tf.keras.utils.load_img(filename, target_size=(256, 256), interpolation='bilinear')
     image = np.array(image) / 255
@@ -19,20 +19,47 @@ def get_dataset_item(filename):
 
     return image, label
 
-def generate_dataset(filenames):
+def generate_exposure_dataset(filenames):
     for filename in filenames:
-        yield get_dataset_item(filename)
+        yield get_exposure_dataset_item(filename)
 
 
-def load_dataset(folder):
+def load_exposure_dataset(folder):
     label_files = [os.path.join(folder, f) for f in os.listdir(folder)]
     dataset = tf.data.Dataset.from_generator(
-        generate_dataset,
+        generate_exposure_dataset,
         args=(label_files,),
         output_signature=(
             tf.TensorSpec(shape=(256, 256, 3), dtype=tf.float32),
             tf.TensorSpec(shape=(), dtype=tf.float32),
-        )
+        ),
     )
 
     return dataset
+
+def get_realblur_dataset_item(filename, label):
+    # Load image
+    image = tf.keras.utils.load_img(filename)
+    image = np.array(image) / 255
+
+    return image, label
+
+def generate_realblur_dataset(list_file):
+    base_dir = os.path.dirname(list_file)
+    with open(list_file, 'r') as f:
+        for line in f.readlines():
+            gt_filename, blurred_filename, *_ = line.split(' ')
+            yield get_blur_dataset_item(os.path.join(base_dir, blurred_filename.encode('utf-8')), 1)
+            yield get_blur_dataset_item(os.path.join(base_dir, gt_filename.encode('utf-8')), 0)
+
+def load_realblur_dataset(list_file):
+        dataset = tf.data.Dataset.from_generator(
+            generate_blur_dataset,
+            args=(list_file,),
+            output_signature=(
+                tf.TensorSpec(shape=(None, None, 3), dtype=tf.float32),
+                tf.TensorSpec(shape=(), dtype=tf.float32),
+            ),
+        )
+
+        return dataset
--- main.py
+++ main.py
@@ -1,17 +1,48 @@
 import os
 import matplotlib.pyplot as plt
 import tensorflow as tf
-from data_loader import load_dataset
-from model import ImageRegression
+from data_loader import load_exposure_dataset, load_realblur_dataset
+from model import ImageRegression, ImageBinaryClassification
 
-TRAINING_DATASET_FOLDER = r'E:\photography\exposure\training\INPUT_IMAGES'
-VALIDATION_DATASET_FOLDER = r'E:\photography\exposure\validation\INPUT_IMAGES'
-TESTING_DATASET_FOLDER = r'E:\photography\exposure\testing\INPUT_IMAGES'
+EXPOSURE_TRAINING_DATASET_FOLDER = r'E:\photography\exposure\training\INPUT_IMAGES'
+EXPOSURE_VALIDATION_DATASET_FOLDER = r'E:\photography\exposure\validation\INPUT_IMAGES'
+EXPOSURE_TESTING_DATASET_FOLDER = r'E:\photography\exposure\testing\INPUT_IMAGES'
 
-training_dataset = load_dataset(TRAINING_DATASET_FOLDER)
-validation_dataset = load_dataset(VALIDATION_DATASET_FOLDER)
-testing_dataset = load_dataset(TESTING_DATASET_FOLDER)
+BLUR_TRAINING_DATASET_FILE = r'E:\photography\blur\RealBlur_J_train_list.txt'
+BLUR_TESTING_DATASET_FILE = r'E:\photography\blur\RealBlur_J_test_list.txt'
 
-model = ImageRegression()
-model.train(training_dataset, validation_dataset, 50)
-# model.test(testing_dataset)
+def run_exposure_model(train=True):
+    augmentations = tf.keras.Sequential([
+        tf.keras.layers.RandomFlip('horizontal'),
+        tf.keras.layers.RandomRotation(0.1),
+    ])
+    exposure_training_dataset = load_exposure_dataset(EXPOSURE_TRAINING_DATASET_FOLDER)
+    exposure_training_dataset = exposure_training_dataset.map(lambda x, y: (augmentations(x), y))
+    exposure_validation_dataset = load_exposure_dataset(EXPOSURE_VALIDATION_DATASET_FOLDER)
+    exposure_testing_dataset = load_exposure_dataset(EXPOSURE_TESTING_DATASET_FOLDER)
+
+    model = ImageRegression(augmentations)
+    if train:
+        model.train(exposure_training_dataset, exposure_validation_dataset, 50)
+    model.test(exposure_testing_dataset)
+
+def run_blur_model(train=True):
+    augmentations = tf.keras.Sequential([
+        tf.keras.layers.RandomCrop(512, 512),
+        tf.keras.layers.RandomFlip('horizontal'),
+    ])
+
+    blur_training_dataset = load_realblur_dataset(BLUR_TRAINING_DATASET_FILE)
+    blur_training_dataset = blur_training_dataset.map(lambda x, y: (augmentations(x), y))
+    blur_testing_dataset = load_realblur_dataset(BLUR_TESTING_DATASET_FILE)
+    blur_testing_dataset = blur_testing_dataset.map(lambda x, y: (tf.image.crop_to_bounding_box(x, 0, 0, 512, 512), y))
+
+    model = ImageBinaryClassification(1)
+    if train:
+        model.train(blur_training_dataset, blur_testing_dataset, 50)
+    model.test(blur_testing_dataset)
+
+if __name__ == '__main__':
+    # run_exposure_model()
+    # TODO: need to make work with any resolution, not just 512x512 - maybe make multiple predictions per image and average?
+    run_blur_model()
