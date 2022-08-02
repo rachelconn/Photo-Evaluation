8dd9200d01c884e313417725251dd9c083f38019
Ryan Conn <rconn478@gmail.com>
2022-08-02 10:01:06 -0500

    Improve model loading

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import os
 from matplotlib import pyplot as plt
 import tensorflow as tf
-from network import ImageRegressor, ImageBinaryClassifier, ResNetImageRegressor, CNNImageRegressor
+from network import ImageRegressor, ImageBinaryClassifier, ResNetImageRegressor, CNNImageRegressor, CNNImageBinaryClassifier
 
 class ImageRegression:
     def __init__(self, batch_size, model_name):
@@ -18,7 +18,7 @@ class ImageRegression:
             optimizer=self.optimizer,
             loss=self.loss,
         )
-        # self.network.summary()
+        self.load()
 
     def train(self, training_dataset, validation_dataset, num_epochs):
         training_dataset = training_dataset.shuffle(10).batch(self.batch_size).prefetch(tf.data.AUTOTUNE)
@@ -45,7 +45,10 @@ class ImageRegression:
             if is_best:
                 best_val_loss = val_loss
                 print(f'Achieved new best validation loss ({val_loss}), saving...')
-                self.save(epoch)
+                self.save(f'epoch_{epoch}')
+
+        # Done training, save final model
+        self.save('final')
 
     def test(self, testing_dataset):
         testing_dataset = testing_dataset.batch(1)
@@ -55,13 +58,13 @@ class ImageRegression:
             plt.title(f'Real: {exposure[0]}\nPredicted: {pred.numpy()[0][0]:3f}')
             plt.show()
 
-    def save(self, epoch):
-        path = os.path.join('trained', self.model_name, f'epoch_{epoch}', 'model')
+    def save(self, name):
+        path = os.path.join('trained', self.model_name, name, 'model')
         self.network.save_weights(path)
         print(f'Saved model to {path}.')
 
     def load(self):
-        path = os.path.join('trained', 'model', 'model')
+        path = os.path.join('trained', self.model_name, 'final', 'model')
         try:
             self.network.load_weights(path)
             print(f'Loaded existing model from {path}')
@@ -70,34 +73,32 @@ class ImageRegression:
             return
 
 class ImageBinaryClassification(ImageRegression):
+    """
+    Cats + Dogs
+        LR for CNN:
+            0.0005 (0.79 acc)
+        LR for resnet:
+            0.0005 (0.53 acc)
+            0.00025 (0.57)
+            0.00001 (each epoch: 0.56, 0.62, 0.68, 0.75)
+    Blur
+        CNN:
+            0.00025 (0.58)
+            0.0001  (0.61)
+        Resnet:
+            0.000001 (0.5, not just guessing one label)
+            0.0000005 (same)
+    RealBlur
+        CNN:
+            0.00005 (0.90)
+    """
+
     def __init__(self, batch_size, model_name, use_resnet=True):
         super().__init__(batch_size, model_name)
         self.batch_size = batch_size
 
-        if use_resnet:
-            self.network = CNNImageRegressor()
-        else:
-            self.network = ImageBinaryClassifier(512)
+        self.network = CNNImageBinaryClassifier()
 
-        """
-        Cats + Dogs
-            LR for CNN:
-                0.0005 (0.79 acc)
-            LR for resnet:
-                0.0005 (0.53 acc)
-                0.00025 (0.57)
-                0.00001 (each epoch: 0.56, 0.62, 0.68, 0.75)
-        Blur
-            CNN:
-                0.00025 (0.58)
-                0.0001  (0.61)
-            Resnet:
-                0.000001 (0.5, not just guessing one label)
-                0.0000005 (same)
-        RealBlur
-            CNN:
-                0.00005 (0.90)
-        """
         self.optimizer = tf.keras.optimizers.Adam(
             learning_rate=0.00005,
         )
