e22d12453cc999b79740ccc0c26f0dd1f811c106
Ryan Conn <rconn478@gmail.com>
2022-08-07 19:47:34 -0500

    Create API for evaluating base64-encoded JPEG

--- evalserver/evalserver/settings.py
+++ evalserver/evalserver/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'eval.apps.EvalConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
---
+++ evalserver/evalserver/views.py
@@ -0,0 +1,33 @@
+import sys
+from os import path
+
+import numpy as np
+from rest_framework.decorators import api_view, parser_classes
+from rest_framework.response import Response
+import tensorflow as tf
+
+sys.path.append(path.abspath(path.join(path.dirname(__file__), '..', '..')))
+from model import ImageRegression, ImageBinaryClassification
+
+exposure_model = ImageRegression(1, 'exposure').network
+blur_model = ImageBinaryClassification(1, 'blur').network
+noise_model = ImageBinaryClassification(1, 'noise').network
+
+def process_request(request):
+    image = tf.constant(request.data['image'])
+    image = tf.io.decode_base64(image)
+    image = tf.io.decode_jpeg(image)
+    image = tf.cast(image, 'float32') / 255
+    return np.array([image])
+
+# Create your views here.
+@api_view(['POST'])
+def evaluate_photo(request):
+    image = process_request(request)
+    response = {
+        'exposure': exposure_model.predict(image),
+        'blur': blur_model.predict(image),
+        'noise': noise_model.predict(image),
+    }
+
+    return Response(response)
