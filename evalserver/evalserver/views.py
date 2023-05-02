aa69ac7fc8822e9a11b26d00a85e69f9fa7b1d1d
Rachel Conn <rconn478@gmail.com>
2023-05-02 13:19:03 -0500

    Fix focus model image generation

--- evalserver/evalserver/views.py
+++ evalserver/evalserver/views.py
@@ -4,7 +4,7 @@ from os import path
 import sys
 
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageOps
 from rest_framework.decorators import api_view, parser_classes
 from rest_framework.response import Response
 import tensorflow as tf
@@ -16,6 +16,9 @@ sys.path.append(path.abspath(path.join(path.dirname(__file__), '..', '..')))
 from model import ImageRegression, ImageBinaryClassification
 from beit.utils import load_model
 from beit.dataset import resize
+from emanet.network import EMANet
+from emanet import settings as emanet_settings
+from matplotlib import pyplot as plt
 
 # Create tf configuration with dynamic memory allocation
 physical_devices = tf.config.list_physical_devices('GPU')
@@ -26,28 +29,40 @@ blur_model = ImageBinaryClassification(1, 'blur').network
 noise_model = ImageBinaryClassification(1, 'noise').network
 bokeh_model = ImageBinaryClassification(1, 'bokeh').network
 
-focus_model, *_ = load_model('eval')
+focus_model = EMANet(emanet_settings.N_CLASSES, emanet_settings.N_LAYERS).cuda()
+state_dict = torch.load(emanet_settings.MODEL_DIR / 'final.pth', map_location=lambda storage, loc: storage.cuda())
+focus_model.load_state_dict(state_dict['net'])
 focus_model.eval()
-focus_processor = BeitImageProcessor()
 
 def process_request(request):
     """ Returns (tf_tensor, torch_tensor) for a given request's image """
-    # print(request.data['image'][:100])
-    image = tf.constant(request.data['image'])
-    image = tf.io.decode_base64(image)
-    image = tf.io.decode_jpeg(image)
-    image = tf.cast(image, 'float32') / 255
-    torch_image = torch.unsqueeze(torch.from_numpy(image.numpy()), 0).cuda()
-    return np.array([image]), torch.permute(torch_image, (0, 3, 1, 2))
+    image_data = base64.b64decode(request.data['image'])
+    image_data = BytesIO(image_data)
+    with Image.open(image_data, formats=['JPEG']) as image:
+        image = ImageOps.exif_transpose(image)
+        image = np.asarray(image)
+        image = tf.convert_to_tensor(image)
+        image = tf.cast(image, 'float32') / 255
+        torch_image = torch.unsqueeze(torch.from_numpy(image.numpy()), 0).cuda()
+        return np.array([image]), torch.permute(torch_image, (0, 3, 1, 2))
 
 def get_focus_image(torch_image: torch.Tensor) -> str:
     """ Returns a base64-encoded image of the focus values given the torch tensor for an image """
-    focus = focus_model(pixel_values=resize(torch_image))
-    focus = torch.squeeze(torch.argmax(focus.logits, dim=1)).type(torch.ByteTensor)
+    torch_image = torch.nn.functional.interpolate(
+        torch_image,
+        scale_factor=0.5,
+        mode='nearest-exact',
+    )
+    logits = focus_model(torch_image)
+    # Get predicted classes
+    focus = torch.squeeze(torch.argmax(logits, dim=1)).type(torch.ByteTensor)
+
+    # Convert to image
     focus_image = Image.fromarray(focus.cpu().numpy(), mode='P')
+    # TODO: calculate how much of image is in/out of focus and add to API response
     focus_image.putpalette([
-        255, 0, 0,   # Out of focus: red
         0, 212, 102, # In focus: green
+        255, 0, 0,   # Blurred: red
     ])
     focus_file = BytesIO()
     focus_image.save(focus_file, format="PNG")
@@ -60,11 +75,11 @@ def evaluate_photo(request):
     focus_image = get_focus_image(torch_image)
 
     response = dict(
-        exposure=exposure_model.predict(tf_image)[0][0],
         blur=blur_model.predict(tf_image)[0][0],
-        noise=noise_model.predict(tf_image)[0][0],
         bokeh=bokeh_model.predict(tf_image)[0][0],
+        exposure=exposure_model.predict(tf_image)[0][0],
         focus=focus_image,
+        noise=noise_model.predict(tf_image)[0][0],
     )
 
     return Response(response)
